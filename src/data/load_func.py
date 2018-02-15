ec82589c427072f8c2626f35211dbab42ccc3c39
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2018-02-15 19:15:25 -0200

    code adaptation to enable processing of months of traffic data

--- src/data/load_func.py
+++ src/data/load_func.py
@@ -3,80 +3,95 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-from sqlalchemy import extract, select
+from sqlalchemy import extract, select, func
 from sqlalchemy.sql import or_, and_
 import datetime
 from shapely.geometry import Point
 
 from src.data.processing_func import (get_direction, extract_geo_sections)
 
-def extract_jps(meta, date_begin, date_end, periods=None, weekends=False, summary=False):
-  start = time.time()
+def extract_jps(meta, date_begin, date_end, periods=None, weekends=False,
+                summary=False, skip=None, limit=20000, return_count=False):
+    start = time.time()
 
-  jps = meta.tables["JamPerSection"]
-  jam = meta.tables["Jam"]
-  sctn = meta.tables["Section"]
-  mongo_record = meta.tables["MongoRecord"]
+    jps = meta.tables["JamPerSection"]
+    jam = meta.tables["Jam"]
+    sctn = meta.tables["Section"]
+    mongo_record = meta.tables["MongoRecord"]
 
-  query = select([mongo_record.c.MgrcDateStart,
-                  jps.c.JpsId,
-                  jps.c.SctnId,
-                  jam.c.JamId,
-                  jam.c.JamIndLevelOfTraffic,
-                  jam.c.JamQtdLengthMeters,
-                  jam.c.JamSpdMetersPerSecond,
-                  jam.c.JamTimeDelayInSeconds,
-                  jam.c.JamQtdLengthMeters,
-                  jam.c.JamDscCoordinatesLonLat]).\
-                  select_from(mongo_record.join(jam.join(jps), isouter=True)).\
-                  where(mongo_record.c.MgrcDateStart.between(date_begin, date_end))
+    query_count = select([func.count()])
 
-  if not weekends:
-    query = query.where(extract("isodow", mongo_record.c.MgrcDateStart).in_(list(range(1,6))))
+    query_all = select([mongo_record.c.MgrcDateStart,
+                    jps.c.JpsId,
+                    jps.c.SctnId,
+                    jam.c.JamId,
+                    jam.c.JamIndLevelOfTraffic,
+                    jam.c.JamQtdLengthMeters,
+                    jam.c.JamSpdMetersPerSecond,
+                    jam.c.JamTimeDelayInSeconds,
+                    jam.c.JamQtdLengthMeters,
+                    jam.c.JamDscCoordinatesLonLat])
 
-  if periods:
-    or_list=[]
-    for t in periods:
-      or_list.append(and_(extract("hour", mongo_record.c.MgrcDateStart)>=t[0],
-                          extract("hour", mongo_record.c.MgrcDateStart)<t[1]
+    queries = [query_count, query_all]
+
+    queries = [q.select_from(mongo_record.join(jam.join(jps), isouter=True)).\
+                      where(mongo_record.c.MgrcDateStart.between(date_begin, date_end)) for q in queries]
+
+    if not weekends:
+        queries = [q.where(extract("isodow", mongo_record.c.MgrcDateStart).in_(list(range(1,6))))
+                   for q in queries]
+
+    if periods:
+        or_list=[]
+        for t in periods:
+            or_list.append(and_(extract("hour", mongo_record.c.MgrcDateStart)>=t[0],
+                                extract("hour", mongo_record.c.MgrcDateStart)<t[1]
+                                )
                           )
-                    )
-    query = query.where(or_(*or_list))
+        queries = [q.where(or_(*or_list)) for q in queries]
 
-  df_jps = pd.read_sql(query, meta.bind)
-  df_jps["JamSpdKmPerHour"] = df_jps["JamSpdMetersPerSecond"]*3.6
-  df_jps[["LonDirection","LatDirection"]] = df_jps["JamDscCoordinatesLonLat"].apply(get_direction)
-  try:
-    df_jps["MgrcDateStart"] = df_jps["MgrcDateStart"].dt.tz_convert("America/Sao_Paulo")
-  except AttributeError:
-    pass
-  df_jps["date"] = pd.to_datetime(df_jps["MgrcDateStart"], utc=True).dt.date
-  df_jps["hour"] = df_jps["MgrcDateStart"].astype(str).str[11:13].astype(int)
-  df_jps["minute"] = df_jps["MgrcDateStart"].astype(str).str[14:16].astype(int)
-  df_jps["period"] = np.sign(df_jps["hour"]-12)
+    query_count, query_all = queries
 
-  bins = [0, 14, 29, 44, 59]
-  labels = []
-  for i in range(1,len(bins)):
-    if i==1:
-      labels.append(str(bins[i-1]) + " a " + str(bins[i]))
-    else:
-      labels.append(str(bins[i-1]+1) + " a " + str(bins[i]))
+    if return_count:
+        size = query_count.execute().scalar()
+        return size
 
-  df_jps['minute_bin'] = pd.cut(df_jps["minute"], bins, labels=labels, include_lowest=True)
-  end = time.time()
+    query_all = query_all.order_by(mongo_record.c.MgrcDateStart, jps.c.JpsId).offset(skip).limit(limit)
 
-  processing_time = round(end - start)
+    df_jps = pd.read_sql(query_all, meta.bind)
+    df_jps["JamSpdKmPerHour"] = df_jps["JamSpdMetersPerSecond"]*3.6
+    df_jps[["LonDirection","LatDirection"]] = df_jps["JamDscCoordinatesLonLat"].apply(get_direction)
+    try:
+      df_jps["MgrcDateStart"] = df_jps["MgrcDateStart"].dt.tz_convert("America/Sao_Paulo")
+    except AttributeError:
+      pass
+    df_jps["date"] = pd.to_datetime(df_jps["MgrcDateStart"], utc=True).dt.date
+    df_jps["hour"] = df_jps["MgrcDateStart"].astype(str).str[11:13].astype(int)
+    df_jps["minute"] = df_jps["MgrcDateStart"].astype(str).str[14:16].astype(int)
+    df_jps["period"] = np.sign(df_jps["hour"]-12)
 
-  if summary:
-    minutos_engarrafados = df_jps["JamId"].nunique()
-    n_trechos = df_jps["SctnId"].nunique()
+    bins = [0, 14, 29, 44, 59]
+    labels = []
+    for i in range(1,len(bins)):
+      if i==1:
+        labels.append(str(bins[i-1]) + " a " + str(bins[i]))
+      else:
+        labels.append(str(bins[i-1]+1) + " a " + str(bins[i]))
 
-    print("Tempo para carregamento dos dados: " + str(processing_time) + " segundos.")
-    print("Minutos de engarrafamento carregados: " + str(minutos_engarrafados))
-    print("Número de trechos abrangidos: " + str(n_trechos))
+    df_jps['minute_bin'] = pd.cut(df_jps["minute"], bins, labels=labels, include_lowest=True)
+    end = time.time()
 
-  return df_jps
+    processing_time = round(end - start)
+
+    if summary:
+      minutos_engarrafados = df_jps["JamId"].nunique()
+      n_trechos = df_jps["SctnId"].nunique()
+
+      print("Tempo para carregamento dos dados: " + str(processing_time) + " segundos.")
+      print("Minutos de engarrafamento carregados: " + str(minutos_engarrafados))
+      print("Número de trechos abrangidos: " + str(n_trechos))
+
+    return df_jps
 
 def transf_flow_features(df_jps):
   df_flow_features = df_jps.groupby(["SctnId", "date", "hour",
@@ -115,14 +130,16 @@ def transf_flow_labels(meta, path_fluxos):
             "Leste": "East",
             "Oeste": "West",}
   df_fluxos["Direction"] = df_fluxos["Sentido"].str.split("/", 1).str.get(1).map(direction)
-  df_fluxos["date"] = pd.to_datetime(df_fluxos["Data"], dayfirst=True)
+  df_fluxos["date"] = pd.to_datetime(df_fluxos["Data"], dayfirst=True).dt.date
   
   geo_fluxos = gpd.GeoDataFrame(df_fluxos, crs={'init': 'epsg:4326'}, geometry="fluxo_Point")
   df_flow_labels = gpd.sjoin(geo_fluxos, geo_sections, how="left", op="within")
   df_flow_labels["hour"] = df_flow_labels["Horario"].str[:2].astype(int)
   df_flow_labels["minute_bin"] = df_flow_labels["Horario"].str[3:5] + " a " + df_flow_labels["Horario"].str[12:14]
   df_flow_labels["minute_bin"] = df_flow_labels["minute_bin"].str.replace("00", "0")
-  df_flow_labels.set_index(["SctnId", "date", "hour", "minute_bin", "Direction"], inplace=True)
+  df_flow_labels.set_index("date", inplace=True) #Done separately because set_index to Multiindex convert date type to Timestamp
+  df_flow_labels.set_index(["SctnId", "hour", "minute_bin", "Direction"], append=True, inplace=True)
+  df_flow_labels.index = df_flow_labels.index.swaplevel(0,1) #So index will be in the same order as df_flow_features
   columns = ['Endereco', 'Corredor', 'Ciclofaixa', 'Numero de faixas', 'Sentido', 'Equipamento', '00 a 10',
              '11 a 20', '21 a 30', '31 a 40', '41 a 50', '51 a 60', '61 a 70',
              '71 a 80', '81 a 90', '91 a 100', 'Acima de 100', 'Total',
--- src/data/make_flow_dataset.py
+++ src/data/make_flow_dataset.py
@@ -3,7 +3,7 @@ import sys
 project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
 sys.path.append(project_dir)
 
-import time
+import math
 import pandas as pd
 import geopandas as gpd
 from sqlalchemy import MetaData, create_engine, extract, select
@@ -12,6 +12,8 @@ from sqlalchemy.sql import or_
 import datetime
 from pytz import timezone
 
+from timeit import default_timer as timer
+
 from src.data.processing_func import (get_direction, connect_database)
 from src.data.load_func import extract_jps, transf_flow_features, transf_flow_labels
 
@@ -19,39 +21,80 @@ import dotenv
 dotenv_path = os.path.join(project_dir, '.env')
 dotenv.load_dotenv(dotenv_path)
 
+rewrite_files = None
+while (rewrite_files != "Y") and (rewrite_files != "N") and (rewrite_files != "y") and (rewrite_files != "n"):
+    rewrite_files = input("Do you wish overwrite flow_dataset files? (Y/N): ")
+if (rewrite_files == "N") or (rewrite_files == "n"):
+    print("Successfully exiting program.")
+    sys.exit()
+elif (rewrite_files == "Y") or (rewrite_files == "y"):
+    pass
+
 #Connection and initial setup
 meta = connect_database()
 
-date_begin = datetime.date(day=15, month=10, year=2017)
-date_end = datetime.date(day=20, month=10, year=2017)
+date_begin = datetime.date(day=1, month=9, year=2017)
+date_end = datetime.date(day=31, month=1, year=2018)
 
-df_jps = extract_jps(meta, date_begin, date_end, weekends=True, summary=True)
-df_flow_features = transf_flow_features(df_jps)
-path_fluxos = project_dir + "/data/external/fotosensores_Fluxo_veiculos.csv"
-df_flow_labels = transf_flow_labels(meta, path_fluxos)
+total_rows = extract_jps(meta, date_begin, date_end, weekends=True, summary=True, return_count=True)
+batch_size = 50000
+number_batches = math.ceil(total_rows / batch_size)
 
-df_flow_features_NS = df_flow_features.reset_index(level="LonDirection")
-df_flow_features_NS.index.rename("Direction", level="LatDirection", inplace=True)
+for i in range(0, number_batches):
+    start = timer()
 
-df_flow_features_LW = df_flow_features.reset_index(level="LatDirection")
-df_flow_features_LW.index.rename("Direction", level="LonDirection", inplace=True)
+    df_jps = extract_jps(meta, date_begin, date_end, weekends=True, summary=True, skip=i*batch_size, limit=batch_size)
+    df_flow_features = transf_flow_features(df_jps)
+    path_fluxos = project_dir + "/data/external/fotosensores_Fluxo_veiculos.csv"
+    df_flow_labels = transf_flow_labels(meta, path_fluxos)
 
-try:
-	df_flow_dataset_NS = df_flow_features_NS.merge(df_flow_labels, how="inner", left_index=True, right_index=True)
-	df_flow_dataset_NS.reset_index(inplace=True)
-except TypeError as e:
-	print(str(e))
+    df_flow_features_NS = df_flow_features.reset_index(level="LonDirection")
+    df_flow_features_NS.index.rename("Direction", level="LatDirection", inplace=True)
 
-try:
-	df_flow_dataset_LW = df_flow_features_LW.merge(df_flow_labels, how="inner", left_index=True, right_index=True)
-	df_flow_dataset_LW.reset_index(inplace=True)
-except TypeError  as e:
-	print(str(e))
+    df_flow_features_LW = df_flow_features.reset_index(level="LatDirection")
+    df_flow_features_LW.index.rename("Direction", level="LonDirection", inplace=True)
 
-if ('df_flow_dataset_NS' in globals()) & ('df_flow_dataset_LW' in globals()):
-	flow_dataset = df_flow_dataset_NS.append(df_flow_dataset_LW, ignore_index=True)
-	flow_dataset.to_csv(project_dir + "/data/processed/flow_dataset.csv")
-elif 'df_flow_dataset_NS' in globals():
-	df_flow_dataset_NS.to_csv(project_dir + "/data/processed/flow_dataset.csv")
-elif 'df_flow_dataset_LW' in globals():
-	df_flow_dataset_LW.to_csv(project_dir + "/data/processed/flow_dataset.csv")
+    df_flow_dataset_NS = None
+    df_flow_dataset_LW = None
+
+    try:
+        df_flow_dataset_NS = df_flow_features_NS.merge(df_flow_labels, how="inner", left_index=True, right_index=True)
+        df_flow_dataset_NS.reset_index(inplace=True)
+    except TypeError as e:
+        print("df_flow_dataset_NS:")
+        print(str(e))
+
+    try:
+        df_flow_dataset_LW = df_flow_features_LW.merge(df_flow_labels, how="inner", left_index=True, right_index=True)
+        df_flow_dataset_LW.reset_index(inplace=True)
+    except TypeError as e:
+        print("df_flow_dataset_LW:")
+        print(str(e))
+
+    if df_flow_dataset_NS & df_flow_dataset_LW:
+        flow_dataset = df_flow_dataset_NS.append(df_flow_dataset_LW, ignore_index=True)
+    elif df_flow_dataset_NS:
+        flow_dataset = df_flow_dataset_NS
+    elif df_flow_dataset_LW:
+        flow_dataset = df_flow_dataset_LW
+    else:
+        end = timer()
+        duration = str(round(end - start))
+        print("Batch " + str(i+1) + " of " + str(number_batches) + \
+              " took " + duration + "s to process but no file was saved, proceeding to next loop.")
+        continue
+
+    num_matches = len(flow_dataset)
+    flow_dataset.to_csv(project_dir + "/data/processed/flow_dataset_" + str(i+1) + ".csv")
+    print("Batch " + str(i+1) + " of " + str(number_batches) + " took " + \
+          duration + "s to process " + \
+          str(num_matches) + \
+          " matches, and flow_dataset_" + \
+          str(i) + ".csv was successfully stored")
+
+
+
+
+
+
+
