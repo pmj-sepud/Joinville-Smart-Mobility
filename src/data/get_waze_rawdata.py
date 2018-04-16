def4962f38cdfb73010ac3f2563804ec9369c3f5
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2018-04-16 10:48:20 -0300

    improvements in get_waze_rawdata, for more robust data sync from mongodb

--- src/data/get_waze_rawdata.py
+++ src/data/get_waze_rawdata.py
@@ -25,21 +25,23 @@ args = parser.parse_args()
 if args.batchsize:
 	batch_size = args.batchsize
 else:
-	batch_size = 20000
+	batch_size = 200
 
 update = args.update
 
 all_files = os.listdir(project_dir+"/data/raw/")
-all_files = [file for file in all_files if "_.txt" in file]
+all_files = [file for file in all_files if "wazerawdata" in file]
+all_files.sort()
 
 num_docs = 0
 if update:
     for file in all_files:
         os.remove(project_dir + "/data/raw/" + file)
 else:
-    for file in all_files:
-        file_info = file.split("_")
-        num_docs += int(file_info[-2])
+    if all_files[-1].split("_")[-2] == all_files[-1].split("_")[-4]:
+        os.remove(project_dir + "/data/raw/" + all_files[-1])
+    num_docs = int(all_files[-1].split("_")[-4])
+
 
 #MongoDB Connection
 uri = os.environ.get("mongo_uri")
@@ -50,23 +52,24 @@ collection = db.ccp_collection
 #Divide the code by batches
 total_rows = collection.count()
 number_batches = math.ceil(total_rows / batch_size)
-width = len(str(number_batches))
+width = len(str(total_rows))
 
 for i in range(0, number_batches):
     start = timer()
     #Fetch documents
-    records = list(collection.find({}, skip=(num_docs + batch_size*i), limit=batch_size, sort=[("_id", ASCENDING)]))
+    records = list(collection.find({}, skip=(num_docs), limit=batch_size, sort=[("_id", ASCENDING)]))
+    now_processed = num_docs + batch_size
 
-    with open(project_dir+"/data/raw/wazerawdata_" + str(i+1).zfill(width) +
-             "_of_" + str(number_batches) + "_batch_" + str(batch_size) + 
+    with open(project_dir+"/data/raw/wazerawdata_" + str(num_docs).zfill(width) +
+             "_to_" + str(now_processed).zfill(width) + "_of_" + str(total_rows) + 
              "_.txt", 'w') as outfile:
         json_record = dumps(records)
         json.dump(json_record, outfile)
 
     end = timer()
     duration = str(round(end - start))
-    print("Batch " + str(i+1) + " of " + str(number_batches) + " took " + duration + " s to be successfully stored.")
-
+    print(str(now_processed), "rows of", str(total_rows) + ", took", duration, "s to be successfully stored.")
+    num_docs = now_processed
 
 
 
